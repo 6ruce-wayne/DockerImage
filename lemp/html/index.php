00f0a27eb1ac96f120b92a7c2f814bcbeab3ebd7
6ruce-wayne <rmninthailand@hotmail.com>
2018-12-20 12:06:53 +0700

    Ver1.1

--- lemp/html/index.php
+++ lemp/html/index.php
@@ -5,13 +5,13 @@
 
    <body>
    <?php
-$mysqli = new mysqli("localhost", "root", "123132123", "test");
+$mysqli = new mysqli("db", "root", "romn2011.", "lemp");
 if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
 echo $mysqli->host_info . "\n";
 
-$mysqli = new mysqli("127.0.0.1", "user", "123132123", "test", 3306);
+$mysqli = new mysqli("db", "root", "romn2011.", "lemp", 3306);
 if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
